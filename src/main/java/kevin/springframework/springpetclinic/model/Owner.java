d86b04b5af99754ad4daae94a90efbc7054d3830
kevinanm <kevin.moreno@openmarket.com>
2021-06-14 13:57:35 -0500

    adding POJOs. Closes #2.

---
+++ src/main/java/kevin/springframework/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package kevin.springframework.springpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/kevin/springframework/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package kevin.springframework.springpetclinic.model;
+
+public class Vet extends Person{
+}
