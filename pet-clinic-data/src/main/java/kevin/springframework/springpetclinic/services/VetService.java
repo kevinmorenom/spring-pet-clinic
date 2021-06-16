a0078f29f2971eb965da9a420607200f70d49f6c
kevinanm <kevin.moreno@openmarket.com>
2021-06-16 15:44:23 -0500

    Implement Map Based Services. Closes #6. Closes #8.

--- pet-clinic-data/src/main/java/kevin/springframework/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/kevin/springframework/springpetclinic/services/VetService.java
@@ -5,13 +5,6 @@ import kevin.springframework.springpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findByLastName(String lastName);
-
-    Vet findById(Long id);
-
-    Vet save (Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long> {
 
 }
