e40cc29a970337344bd339bbe800ba5f9482fbed
kevinanm <kevin.moreno@openmarket.com>
2021-06-17 11:39:20 -0500

    Vet View and Controller. Closes #11.

--- pet-clinic-web/src/main/java/kevin/springframework/springpetclinic/controllers.java
+++
@@ -1,4 +0,0 @@
-package kevin.springframework.springpetclinic;
-
-public class controllers {
-}
--- pet-clinic-web/src/main/java/kevin/springframework/springpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/kevin/springframework/springpetclinic/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package kevin.springframework.springpetclinic;
+package kevin.springframework.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
